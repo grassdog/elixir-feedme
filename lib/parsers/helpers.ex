6031d8ada9ee96f6aec442705fee6085572d164d
Ray Grasso <ray.grasso@gmail.com>
2016-07-24 21:47:11 +0800

    Add some extra date formats

--- lib/parsers/helpers.ex
+++ lib/parsers/helpers.ex
@@ -2,13 +2,29 @@ defmodule Feedme.Parsers.Helpers do
   alias Feedme.XmlNode
   alias Timex.Parse.DateTime.Parser
 
+  @date_formats [
+    "{RFC1123}",
+    "{ISO:Extended}",
+    "{D} {Mshort} {YYYY} {h24}:{m} {Zname}", # 1 Jul 2016 12:00 GMT
+    "{WDshort} {D} {Mshort} {YYYY} {h24}:{m}:{s} {AM} {Zname}", # Fri 22 Jul 2016 11:25:39 AM CDT
+  ]
+
   def parse_datetime(node), do: parse_datetime(node, "{RFC1123}")
   def parse_datetime(nil, _), do: nil
   def parse_datetime(node, format) do
-    case XmlNode.text(node) |> Parser.parse(format) do
+    case XmlNode.text(node) |> parse_date do
       {:ok, date} -> date
       _ -> nil
     end
+  end
+
+  defp parse_date(text) do
+    @date_formats
+    |> Enum.map(fn f -> Parser.parse(text, f) end)
+    |> Enum.find(
+    fn {:ok, _} -> true
+      {:error, _} -> false
+    end)
   end
 
   def parse_into_struct(document, struct), do: parse_into_struct(document, struct, [])
